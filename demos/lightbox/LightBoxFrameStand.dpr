0a088b5f7c271d4a7006649d55bb5c7a8826117f
Andrea Magni <andrea.magni@wintech-italia.com>
2015-09-04 02:30:37 +0200

    * lightbox demo (cleanup)

--- demos/lightbox/LightBoxFrameStand.dpr
+++ demos/lightbox/LightBoxFrameStand.dpr
@@ -4,9 +4,9 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Forms.Main in 'Forms.Main.pas' {MainForm},
+  Frames.Dataset in 'Frames.Dataset.pas' {DatasetFrame: TFrame},
   Frames.Picture in 'Frames.Picture.pas' {PictureFrame: TFrame},
-  Frames.Text in 'Frames.Text.Pas' {TextFrame: TFrame},
-  Frames.Dataset in 'Frames.Dataset.pas' {DatasetFrame: TFrame};
+  Frames.Text in 'Frames.Text.Pas' {TextFrame: TFrame};
 
 {$R *.res}
 
